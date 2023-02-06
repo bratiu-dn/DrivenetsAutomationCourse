7015e7ebe60e736fdaaf3c151ba507528d8bbdcb
Bogdan Ratiu <bratiu-dn@drivenets.com>
2023-02-06 11:17:10 +0100

    first commit

---
+++ helloword.py
@@ -0,0 +1,6 @@
+# this is my first code
+print("Hello Word!", "I want this to be on the same line as the one above", sep="!")  # added a sep parameter
+print("I want this to be on the same line as the one above")
+print("Hello Word!", "I want this to be on the same line as the one above", sep="anything")  # copy paste 1
+
+
